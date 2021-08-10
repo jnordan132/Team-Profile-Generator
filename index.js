d8266c90cd0af636f40a4f237df9ab6cef9b6339
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-10 18:08:25 -0400

    made questions array but still need to think about how to do this

--- index.js
+++ index.js
@@ -1,34 +1,75 @@
+// Node modules
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { teamGenerator } = require('./src/teamGenerator');
+const Choice = require('inquirer/lib/objects/choice');
 
+// Team prfile modules
+const { teamGenerator } = require('./src/teamGenerator');
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+
+// Questions array
 const questions = [{
         type: "input",
-        name: "",
-        message: ""
+        name: "manName",
+        message: "Manager name?"
     },
     {
         type: "input",
-        name: "",
-        message: ""
+        name: "manId",
+        message: "What is this managers employee ID?"
     },
     {
         type: "input",
-        name: "",
-        message: ""
+        name: "manEmail",
+        message: "What is this managers email address"
     },
     {
         type: "input",
-        name: "",
-        message: ""
+        name: "manOfficeNum",
+        message: "What is this managers office number?"
+    },
+    {
+        type: "list",
+        name: "engOrInt",
+        message: "Would you like to add an engineer, intern, or finish?",
+        choices: ["Engineer", "Intern", "Finish"]
     },
     {
         type: "input",
-        name: "",
-        message: ""
+        name: "empName",
+        message: `${} name?`
+    },
+    {
+        type: "input",
+        name: "empId",
+        message: `What is this ${} employee ID?`
+    },
+    {
+        type: "input",
+        name: "empEmail",
+        message: `What is this ${} email address?`
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is this engineers GitHub username?",
+        when(input) {
+            input.engOrInt === "Engineer"
+        }
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school does this intern go to?",
+        when(choices) {
+            choices.engOrInt === "Intern"
+        }
     }
 ]
 
+// Write to file
 const writeFile = (fileName, data) => {
     fs.writeFile(fileName, data, function(err) {
         console.log(data),
@@ -36,12 +77,13 @@ const writeFile = (fileName, data) => {
     })
 }
 
-const start = () => {
+// Initialize HTML
+const init = () => {
     inquirer.prompt(questions)
         .then(function(data) {
-            writeFile("index.html", teamGenerator(data));
+            writeFile("./dist/index.html", teamGenerator(data));
             console.log(data)
         })
 }
 
-start();
+init();
