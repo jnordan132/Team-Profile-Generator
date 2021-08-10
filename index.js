6af94e0a841a87c9010ef393e982e0dc33a4ac58
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-10 14:05:45 -0400

    final starting coded created

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const teamGenerator = require('./src/teamGenerator');
+
+const questions = [{
+        type: "input",
+        name: "",
+        message: ""
+    },
+    {
+        type: "input",
+        name: "",
+        message: ""
+    },
+    {
+        type: "input",
+        name: "",
+        message: ""
+    },
+    {
+        type: "input",
+        name: "",
+        message: ""
+    },
+    {
+        type: "input",
+        name: "",
+        message: ""
+    }
+]
+
+const writeFile = (fileName, data) => {
+    fs.writeFile(fileName, data, function(err) {
+        console.log(data),
+            err ? console.log(err) : console.log("Team profile page successfully created!")
+    })
+}
+
+const start = () => {
+    inquirer.prompt(questions)
+        .then(function(data) {
+            writeFile("index.html", teamGenerator(data));
+            console.log(data)
+        })
+}
+
+start();
